8402839fe91ebea11ad14c69b9843f8368858628
A. B. Pancu <58257244+minetrana@users.noreply.github.com>
2023-11-08 10:57:47 +0100

    Update APwebserver.ino
    New code implementation - independent of the previous library used. Dependency on WiFiWebServer by H. Koang has been removed

--- APwebserver.ino
+++ APwebserver.ino
@@ -1,113 +1,61 @@
-#include "defines.h"
-#include "EvnironmentData.ino"
+#include <WiFi.h>
+#include <WebServer.h>
+
+#include <string>
 
 #define BUFFER_SIZE 512
 
-const int led = 2;
+const uint8_t led = 2;
 
-/*#if defined(LED_BUILTIN)
-  const int led = LED_BUILTIN;
-#else
-  #if (ESP32)
-    const int led = 2;
-  #else
-    const int led = 13;
-  #endif
-#endif*/
+const char* ssid  = "FlowerWall"; //connection point name
+const char* pass = "12345671!";  //password
 
-//AP station status
-int status = WL_IDLE_STATUS;
 
-WiFiWebServer server(80);
+IPAddress local_ip(192, 168, 2, 2);
+IPAddress gateway(192, 168, 2, 2);
+IPAddress subnet(255, 255, 255, 0);
 
-void handleRoot() 
-{
-  digitalWrite(led, 1);
-
-  char temp[BUFFER_SIZE];
-  int sec = millis() / 1000;
-  int min = sec / 60;
-  int hr = min / 60;
-  int day = hr / 24;
-
-  hr = hr % 24;
-
-  //startup web page upon accessing the arduino's IP address; contains basic info on the setup components
-  snprintf(temp, BUFFER_SIZE - 1,
-    "<html>\
-    <head>\
-    <meta http-equiv='refresh' content='5'/>\
-    <title>%s</title>\
-    <style>\
-    body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
-    </style>\
-    </head>\
-    <body>\
-    <h1>Hello from %s</h1>\
-    <h3>running WiFiWebServer</h3>\
-    <h3>on %s</h3>\
-    <p>Uptime: %d d %02d:%02d:%02d</p>\
-    </body>\
-    </html>", BOARD_NAME, BOARD_NAME, SHIELD_TYPE, day, hr, min % 60, sec % 60);
-
-  server.send(200, F("text/html"), temp);
-  digitalWrite(led, 0);
-}
+WebServer server(80);
 
 void setup() 
 {
   //serial start
   Serial.begin(115200);
+
   while (!Serial && millis() < 5000)
     delay(200);
 
   Serial.print(F("\nStarting Flower Wall WebServer on "));
-  Serial.print(BOARD_NAME);
+  Serial.print("ESP-WHOOP-32");
   Serial.print(F(" with "));
-  Serial.println(SHIELD_TYPE);
-  Serial.println(WIFI_WEBSERVER_VERSION);
+  Serial.println("no_shield");
 
   //delay for startup
   delay(500);
+  
+  pinMode(led, OUTPUT);
 
-  #if WIFI_USING_ESP_AT  //initialize serial for ESP module
-    EspSerial.begin(115200);
-    WiFi.init(&EspSerial);
+  WiFi.softAP(ssid, pass);
+  WiFi.softAPConfig(local_ip, gateway, subnet);
+  delay(100);
 
-  #endif
-    pinMode(led, OUTPUT);
+  //show login credentials on serial board
+  Serial.print(F("Starting up Access Point with SSID: "));
+  Serial.println(ssid);
+  Serial.print(F("with password: "));
+  Serial.println(pass);
 
-  #if (ESP32 || ESP8266)
-    WiFi.mode(WIFI_AP);
-
-    //show login credentials on serial board
-    Serial.print(F("Starting up Access Point with SSID: "));
-    Serial.println(ssid);
-    Serial.print(F("with password: "));
-    Serial.println(pass);
-
-  #else
-    uint8_t ap_channel = 2;
-
-    status = WiFi.beginAP(ssid, pass, ap_channel);
-
-    if(status != WL_AP_LISTENING)
-    {
-      Serial.println(F("AP creation failed"));
-      while(true);
-    }
-  #endif
-  //delay(500);
+  delay(500);
 
   //all slash command options
-  server.on(F("/"), handleRoot);
+  server.on("/", handle_OnConnect);
   server.onNotFound(handlerFunctionNotFound);
   //server.on(F("/adapt"), handlerAdaptAnimation); //backlog; blocked by "Implement data gathering"
 
   server.begin();
   Serial.print(F("Server started at "));
-  Serial.println(WiFi.localIP());
-  getWiFiStatus();
+  Serial.println(local_ip);
+
 }
 
 void loop() 
@@ -115,20 +63,10 @@ void loop()
   server.handleClient();
 }
 
-void getWiFiStatus() 
-{
-  Serial.println(F("SSID: "));
-  Serial.println(WiFi.SSID());
-
-  IPAddress IP = WiFi.localIP();
-
-  Serial.println(F("IP address: "));
-  Serial.println(IP);
-}
-
 void handle_OnConnect()  //on startup
 {
-  //start basic animation
+  Serial.println("Startup Complete");
+  server.send(200, "text/html", SendHTML());
 }
 
 void handlerFunctionNotFound() 
@@ -148,4 +86,28 @@ void handlerFunctionNotFound()
   server.send(404, F("Text/plain"), message);
 }
 
+String SendHTML(){
 
+  /*int sec = millis() / 1000;
+  int min = sec / 60;
+  int hr = min / 60;
+  int day = hr / 24;
+
+  hr = hr % 24;*/
+
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr +="<title>LED Control</title>\n";
+  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
+  ptr +="p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
+  ptr +="</style>\n";
+  ptr +="</head>\n";
+  ptr +="<body>\n";
+  ptr +="<h1>ESP32 Web Server</h1>\n";
+  ptr +="<h3>Using Access Point(AP) Mode</h3>\n";
+
+  ptr +="</body>\n";
+  ptr +="</html>\n";
+  return ptr;
+}
